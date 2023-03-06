58a243e511de4bcd959c857ef012127130223a24
Viktor Ovcharov <ovcharov.2v@gmail.com>
2023-03-07 00:08:17 +0400

    feat: add Case list page

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -2,20 +2,35 @@ document.addEventListener('DOMContentLoaded', () => {
 	const dropdownList = document.querySelectorAll('.dropdown')
 	if(dropdownList.length) {
 		dropdownList.forEach((dropdown) => {
-			dropdown.addEventListener('click', (evt) => {
-				evt.stopPropagation()
-				if(!evt.target.closest(dropdown.querySelector('.dropdown__list-box'))) {
-					dropdown.classList.toggle('dropdown--active')
-				}
-			})
-			const input = dropdown.querySelector('.dropdown__value')
-			const optionsList = dropdown.querySelectorAll('.dropdown__option')
-			optionsList.forEach((option) => {
-				option.addEventListener('click', (e) => {
-					e.preventDefault();
-					input.value = option.dataset.value
+			// Multi select
+			if(dropdown.classList.contains('dropdown--multi-select')) {
+				const counter = dropdown.querySelector('.dropdown__value-counter')
+				dropdown.addEventListener('click', (evt) => {
+					evt.stopPropagation()
+					if(!evt.target.closest('.dropdown__options-list')) {
+						dropdown.classList.toggle('dropdown--active')
+					}
+					else {
+						const checkedOptions = dropdown.querySelectorAll(':checked').length;
+						counter.innerHTML = checkedOptions.toString()
+					}
 				})
-			})
+			}
+			// Single select
+			else {
+				dropdown.addEventListener('click', (evt) => {
+					evt.stopPropagation()
+					dropdown.classList.toggle('dropdown--active')
+				})
+				const input = dropdown.querySelector('.dropdown__value')
+				const optionsList = dropdown.querySelectorAll('.dropdown__option')
+				optionsList.forEach((option) => {
+					option.addEventListener('click', (e) => {
+						e.preventDefault();
+						input.value = option.dataset.value
+					})
+				})
+			}
 		})
 	}
 
