b996333615ae83ce19fb74ae263a51509d3a428e
Viktor Ovcharov <ovcharov.2v@gmail.com>
2023-06-19 15:12:51 +0400

    Fixes

--- src/js/components/section-terminals.js
+++ src/js/components/section-terminals.js
@@ -4,21 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
 	const slider = document.querySelector('.section-terminals__slider')
 	if(!slider) return
 
-	new Swiper(slider, {
-		modules: [ Navigation, Pagination ],
-		slidesPerView: 1,
-		grabCursor: true,
-		navigation: {
-			nextEl: '.slider__btn--next',
-			prevEl: '.slider__btn--prev',
-		},
-		pagination: {
-			el: '.slider__pagination',
-			clickable: true,
-			bulletActiveClass: 'slider__bullet--active',
-			renderBullet: function (index, className) {
-				return `<button class="slider__bullet swiper-pagination-bullet" aria-label="Страница ${index}"></button>`;
-			},
-		},
-	});
+	const createTerminalSlider = () => {
+		const slides = slider.querySelectorAll('.slider__slide')
+		console.log(slides)
+		if(slides && slides.length > 1) {
+			slider.classList.remove('slider--disabled')
+			new Swiper(slider, {
+				modules: [ Navigation, Pagination ],
+				slidesPerView: 1,
+				grabCursor: true,
+				navigation: {
+					nextEl: '.slider__btn--next',
+					prevEl: '.slider__btn--prev',
+				},
+				pagination: {
+					el: '.slider__pagination',
+					clickable: true,
+					bulletActiveClass: 'slider__bullet--active',
+					renderBullet: function (index, className) {
+						return `<button class="slider__bullet swiper-pagination-bullet" aria-label="Страница ${index}"></button>`;
+					},
+				},
+			});
+		}
+		else {
+			slider.classList.add('slider--disabled')
+		}
+	}
+
+	createTerminalSlider()
 })
